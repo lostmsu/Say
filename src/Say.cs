47314ff786e9c7edec17d6b570971257106de066
Victor Nova <lostfreeman@gmail.com>
2022-10-28 12:00:08 -0700

    it just works

---
+++ src/Say.cs
@@ -0,0 +1,38 @@
+﻿using ManyConsole.CommandLineUtils;
+
+using Say;
+
+ConsoleCommandDispatcher.DispatchCommand(
+    new[] { new SayCommand() },
+    args, Console.Out);
+
+class SayCommand : VoiceCommand
+{
+    bool verbose;
+
+    public SayCommand()
+    {
+        IsCommand("say");
+
+        SkipsCommandSummaryBeforeRunning();
+
+        HasOption("verbose", "Verbose output", v => verbose = v == null || bool.Parse(v));
+
+        AllowsAnyAdditionalArguments("phrases to say aloud");
+    }
+
+    public override int Run(string[] remainingArguments)
+    {
+        if (verbose)
+        {
+            Console.WriteLine($"Rate: {Synth.Rate}, Volume: {Synth.Volume}, Voice: {Synth.Voice.Name}");
+        }
+
+        foreach (string line in remainingArguments)
+        {
+            Synth.Speak(line);
+        }
+
+        return 0;
+    }
+}
---
+++ src/VoiceCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Speech.Synthesis;
+
+using ManyConsole.CommandLineUtils;
+
+namespace Say;
+
+abstract class VoiceCommand : ConsoleCommand
+{
+    protected SpeechSynthesizer Synth { get; } = new();
+
+    protected VoiceCommand()
+    {
+        HasOption("r|rate=", $"{nameof(Synth.Rate)} of speech [-10 .. 10]", rate => Synth.Rate = int.Parse(rate));
+        HasOption("vol|volume=", $"{nameof(Synth.Volume)} of speech [0 .. 100]", volume => Synth.Volume = int.Parse(volume));
+        HasOption("v|voice=", $"Name of the {nameof(Synth.Voice)} to use", voice => Synth.SelectVoice(voice));
+    }
+}
